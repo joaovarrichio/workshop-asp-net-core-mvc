c53b6705858051582decf7f2855db8ee0cd6b79d
Joao Varrichio <jotavarrichio@yahoo.com.br>
2022-07-02 21:57:39 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["email"] = "jotavarrichio@gmail.com";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
